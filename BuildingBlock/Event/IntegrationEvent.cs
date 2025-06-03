f2dc963fe1cfc866e6df9091b6fbdd25f418434a
Sarah Ramadan Fahmy <sarah.fahmi@moj.gov.sa>
2025-06-03 22:10:30 +0300

    Add building block

---
+++ BuildingBlock/Event/IntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace BuildingBlocks.Messaging.Event;
+
+public record IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+    public Guid Id { get; init; }
+    public DateTime CreationDate { get; init; }
+    public string EventType => GetType().Name;
+    public override string ToString() => $"{EventType} - {Id} - {CreationDate}";
+
+}
+
+
+
+
+
